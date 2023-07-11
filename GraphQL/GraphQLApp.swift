09d653c4d4d84c39d311b769c90c51a1738f5bcb
Pradeep Dahiya <pradeepdahiya@businessnext.com>
2023-07-11 19:24:41 +0100

    Initial Commit

---
+++ GraphQL/GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLApp.swift
+//  GraphQL
+//
+//  Created by Pradeep Dahiya on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GraphQLUITests/GraphQLUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GraphQLUITestsLaunchTests.swift
+//  GraphQLUITests
+//
+//  Created by Pradeep Dahiya on 11/07/2023.
+//
+
+import XCTest
+
+final class GraphQLUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
